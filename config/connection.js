3783f6582ce84a73a48fa432c8b9cd5f9e97863b
Julian Hicks <jkballin@gmail.com>
2019-05-04 14:28:07 -0700

    sql

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 var mysql = require("mysql");
 
 if (process.env.CLEARDB_URL) {
-    connection = mysql.createConnection(process.env.CLEARDB_URL);
+    mysql://b65896c035574d:40a43228@us-cdbr-iron-east-02.cleardb.net/heroku_b84cc6620c0e7d2?reconnect=true
+    connection = mysql.createConnection({
+        host: "us-cdbr-iron-east-02.cleardb.net",
+        user: "b65896c035574d",
+        password: "40a43228",
+        database: "heroku_b84cc6620c0e7d2"
+    });
 } else {
     connection = mysql.createConnection({
         host: "localhost",

9edefaa4b234d89e48ba19caceba6722868dcbfb
Julian Hicks <jkballin@gmail.com>
2019-05-05 18:55:19 -0700

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));

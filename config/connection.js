bc8c85fad108a94265437d11d122245536b8aa73
Julian Hicks <jkballin@gmail.com>
2020-08-06 21:10:18 -0700

    audit

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
-
-if (process.env.CLEARDB_DATABASE_URL) {
-    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
-    } else {
+//SET UP DATABASE FOR JAWSDB IN WORKBENCH USING ITS HOST USER ETC. TO BE PUSHED TO HEROKU
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
